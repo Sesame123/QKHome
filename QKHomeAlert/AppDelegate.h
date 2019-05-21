800ef3b5188ef6efe3ef819269077fdfdbaa9bff
1100114 <zhuzhijie@szqk-tech.com>
2019-05-21 10:33:29 +0800

    更换pod文件位置到根目录

---
+++ QKHomeAlert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QKHomeAlert
+//
+//  Created by admin on 2019/5/20.
+//  Copyright © 2019 qiake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
